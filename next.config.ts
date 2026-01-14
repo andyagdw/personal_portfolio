7ccfcde8350d327e61833c7e3e312b5910d2ce4a
Andy Agyei-Dwumah <andydwumah@gmail.com>
2026-01-14 13:08:14 +0000

    update next.config.ts to set basePath and enable export output

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  basePath: "/personal_portfolio",
+  output: "export",
 };
 
 export default nextConfig;
